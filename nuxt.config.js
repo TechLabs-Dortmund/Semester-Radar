95912fdb0aa4d88ac17978cbdbbb1ddc2511069f
Merphin <delfin.97@gmx.de>
2021-06-10 16:18:11 +0200

    Changed title header

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Radar | ST21 | TechLabs Dortmund 123',
+    titleTemplate: '%s - Radar | ST21 | TechLabs Dortmund',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
