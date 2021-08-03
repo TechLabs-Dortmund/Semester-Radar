f406c36ed9312961ef839cf9b0a4773d72a088dd
TheMerphin <TheMerphin@users.noreply.github.com>
2021-08-03 12:00:28 +0000

    deploy: c0a8416ae477138b5b2efa162528fdda4435d4f9

--- _nuxt/static/1627991817/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627991817",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f4cd2532"}},globalRefs:{}}}(null,"\u002F"));
