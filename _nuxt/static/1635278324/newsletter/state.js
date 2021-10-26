078f8d6813dd2beecb64d3c45347a66be3fcdad4
TheMerphin <TheMerphin@users.noreply.github.com>
2021-10-26 19:58:45 +0000

    deploy: b1f9e58d4b1af0630fc8c3bac43bfdbe46676e30

---
+++ _nuxt/static/1635278324/newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635278324",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnewsletter",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3f2eb026"}},globalRefs:{}}}(null));
