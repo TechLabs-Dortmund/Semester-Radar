17953e2fd70ba8dd37329586976fe5e4e1dc403d
TimoBehrendt <TimoBehrendt@users.noreply.github.com>
2022-11-15 18:35:54 +0000

    deploy: 78330b536e02a533aeae71163080ef19d9ac13b1

---
+++ _nuxt/static/1668537352/media/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668537352",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmedia",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"93e6c767"}},globalRefs:{}}}(null));
