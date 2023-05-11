3a84529bbef0505e847a268a060c731bb4ab5f35
kukli07 <84287747+kukli07@users.noreply.github.com>
2023-05-11 12:30:43 +0200

    Change event dates, Change HTML title to current semester

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Radar | ST22 | TechLabs Dortmund',
+    titleTemplate: '%s - Radar | ST23 | TechLabs Dortmund',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
