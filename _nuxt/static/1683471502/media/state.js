1a9ae958084ed2f66b0c39b72f744ada0572b14b
timemir <timemir@users.noreply.github.com>
2023-05-11 10:12:23 +0000

    deploy: 27d44b41703c41f95fa5c40b4c24cfab16e65437

--- _nuxt/static/1683471502/media/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683471502",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmedia",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e9f0f2b4"}},globalRefs:{}}}(null));
