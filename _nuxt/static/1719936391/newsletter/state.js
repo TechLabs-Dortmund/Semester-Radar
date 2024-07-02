d2d374b277fd37cfec09f02f4e189f8b8f8b973b
TimoBehrendt <TimoBehrendt@users.noreply.github.com>
2024-07-02 16:06:32 +0000

    deploy: efda158f8fddffd683e09456408a4aa6850b738b

---
+++ _nuxt/static/1719936391/newsletter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1719936391",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnewsletter",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cfa3cc56"}},globalRefs:{}}}(null));
